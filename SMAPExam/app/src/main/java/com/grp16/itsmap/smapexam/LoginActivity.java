9c433fd2e1e49634a6c5b3246d65e0d4e90ded7f
jacobboes <jacobboeseriksen@hotmail.com>
2017-05-16 11:57:19 +0200

    login activity

--- SMAPExam/app/src/main/java/com/grp16/itsmap/smapexam/LoginActivity.java
+++ SMAPExam/app/src/main/java/com/grp16/itsmap/smapexam/LoginActivity.java
@@ -3,11 +3,11 @@ package com.grp16.itsmap.smapexam;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class PlaceholderActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_placeholder);
+        setContentView(R.layout.activity_login);
     }
 }
