ba2f39444a53b210e955d3499ffcfaf63a098836
Morten <me@mbork.dk>
2017-05-17 11:04:14 +0200

    Updated POI

--- SMAPExam/app/src/main/java/com/grp16/itsmap/smapexam/model/POI.java
+++ SMAPExam/app/src/main/java/com/grp16/itsmap/smapexam/model/POI.java
@@ -4,22 +4,25 @@ import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class POI {
+    public String uid;
     public double latitude;
     public double longitude;
     public double altitude;
     public String name;
     public String description;
+    public String type;
 
     public POI() {
 
     }
 
-    public POI(double latitude, double longitude, double altitude, String name, String description) {
-
+    public POI(String uid, double latitude, double longitude, double altitude, String name, String description, String type) {
+        this.uid = uid;
         this.latitude = latitude;
         this.longitude = longitude;
         this.altitude = altitude;
         this.name = name;
         this.description = description;
+        this.type = type;
     }
 }
